f67194aa92ae1fc7545f8efa5f31913f02679dd9
jmen <jacky33men@gmail.com>
2025-08-04 22:26:24 -0400

    convert process.env.port to int because app.listen expects a num

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use("/api", artworkRouter);
 app.use("/api/profile", profileRouter);
 
-const PORT = process.env.PORT || 4000;
+const PORT = parseInt(process.env.PORT || '4000', 10);
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Listening on 0.0.0.0:${PORT}`);
 });
