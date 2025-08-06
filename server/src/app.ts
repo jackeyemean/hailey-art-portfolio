ea685b935d9fdd19ee84a4ec9ade5dff0c9c56a2
jmen <jacky33men@gmail.com>
2025-08-05 20:13:48 -0400

    add cors for web admin

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import artworkRouter from "./routes/artwork";
 import profileRouter from "./routes/profile";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", artworkRouter);
