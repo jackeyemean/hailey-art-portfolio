268612ece72613d747c77e6e3f59f3638e89294b
jmen <jacky33men@gmail.com>
2025-08-06 23:45:56 -0400

    bring about/contact down to align end hailey tai

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="px-4 lg:px-16 py-6">
-      <div className="w-full flex justify-between items-center">
+      <div className="w-full flex justify-between items-end">
         <div className="heading-medium">
           Hailey Tai
         </div>
