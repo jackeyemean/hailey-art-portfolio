b84459016acd3f1a639564ab06df6662a1753f91
jmen <jacky33men@gmail.com>
2025-09-13 13:36:26 -0400

    new fonts, better collection card text spacing

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -13,6 +13,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Inter:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400&family=Nanum+Myeongjo:wght@300;400&display=swap" rel="stylesheet" />
+      </head>
       <body className="bg-light-gray">{children}</body>
     </html>
   )
